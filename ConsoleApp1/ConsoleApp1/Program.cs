913942beeb3e49d3df3912dd93db40ba58f84cad
Tova Bienstock <37345644900@mby.co.il>
2024-06-07 09:52:12 +0300

    wow

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Tova!");
+Console.WriteLine("Hello, Tova!:)");
 Console.WriteLine("Hello, Tehila!");
+Console.WriteLine("Hello, Tirtza!");
 
 
