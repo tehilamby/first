fedc3c1e8d6dcb036426c9901ee282623b3ea052
Tova Bienstock <37345644900@mby.co.il>
2024-06-07 10:05:15 +0300

    aa

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, Tova!:)");
-Console.WriteLine("Hello, Tirtza!");
+﻿
 
 
