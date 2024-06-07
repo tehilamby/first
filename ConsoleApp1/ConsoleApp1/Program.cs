d466ba77fc516f074dc75bce02fda45ea9399d33
Tirtza Licht <37325924926@mby.co.il>
2024-06-07 10:12:37 +0300

    tova

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Nice World!");
-Console.WriteLine("Hello,Tovaaaaaaaaa!");
+Console.WriteLine("Hello,Tova!");
 Console.WriteLine("Hello, Tehila! ");
 
 
